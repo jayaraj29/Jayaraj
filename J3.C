bc87385bc0da7c0db1306d93a56ef18c54fd5dc3
jayaraj29 <35688059+jayaraj29@users.noreply.github.com>
2018-01-29 13:31:52 +0530

    J3.C
    odd or even

---
+++ J3.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+int a;
+printf(" \n Enter the number.....");
+scanf("%d",&a);
+if(a%2==0)
+printf(" \n %d is a even ",a);
+else
+printf(" \n %d is a odd ",a);
+return 0;
+}
