e90e80d9d9fc83a0f568d79e3f4b563a8282a978
jayaraj29 <35688059+jayaraj29@users.noreply.github.com>
2018-01-31 14:42:34 +0530

    J8.C
    sum of number upto N

---
+++ J8.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+int a,b=0,i=1;
+printf(" \n Enrer the sum of number upto....");
+scanf("%d",&a);
+for(i;i<=a;i++)
+{
+b=b+i;
+}
+printf("\n%d is the addition",b);
+return 0;
+}
