436bcc9362fb4cb23ee1f528c2009e011c325f62
jayaraj29 <35688059+jayaraj29@users.noreply.github.com>
2018-02-19 07:55:19 +0000

    J31

--- J31.C
+++ J31.C
@@ -4,11 +4,10 @@
 int main()
 {
    char ch1[100],ch2[100],ch3[100];
-   printf("\n Enter the sentence word by word(3 words)....");
+   printf("\n Enter the sentence (only 3 words)....");
    scanf("%s",&ch1);
    scanf("%s",&ch2);
    scanf("%s",&ch3);
    printf("\n %d",strlen(ch1)+strlen(ch2)+strlen(ch3));
    return 0;
 }
-
