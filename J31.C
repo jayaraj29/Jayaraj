e3c33f64af94f8eee54910a121f65f0c1fd1d8d4
jayaraj29 <35688059+jayaraj29@users.noreply.github.com>
2018-02-19 07:53:55 +0000

    J31

---
+++ J31.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include<conio.h>
+int main()
+{
+   char ch1[100],ch2[100],ch3[100];
+   printf("\n Enter the sentence word by word(3 words)....");
+   scanf("%s",&ch1);
+   scanf("%s",&ch2);
+   scanf("%s",&ch3);
+   printf("\n %d",strlen(ch1)+strlen(ch2)+strlen(ch3));
+   return 0;
+}
+
