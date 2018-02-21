290e817208f262a0676d507d90a1924b8e2d837c
jayaraj29 <35688059+jayaraj29@users.noreply.github.com>
2018-02-21 08:46:59 +0000

    J40

---
+++ J40.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() 
+{
+	int a,b=1,c=1,d,i;
+	printf("\n Enter the number.....");
+	scanf("%d",&a);
+	printf("%d",b);
+    for(i=0;i<=a-2;i++)
+    {
+        d=b+c;
+        printf("\t%d",c);
+        b=c;
+        c=d;
+    }
+return 0;
+}
