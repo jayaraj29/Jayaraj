eb5c6e04c417f2ac16ee3f1ef9ee5eecc092ac7e
jayaraj29 <35688059+jayaraj29@users.noreply.github.com>
2018-02-21 09:19:30 +0000

    J43

---
+++ J43.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a;
+    printf("\n Enter the number......");
+    scanf("%d",&a);
+    if(1<=a&&a<=10)
+    printf("\n Yes");
+    else
+    printf("\n No");
+    return 0;
+}
+    
