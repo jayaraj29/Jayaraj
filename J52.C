8d91694ae32103cb8073054f1b97459c6b20dbeb
jayaraj29 <35688059+jayaraj29@users.noreply.github.com>
2018-02-23 16:47:41 +0530

    J52

---
+++ J52.C
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+ int
+main (void) 
+{
+  
+int a;
+  
+printf ("\n Enter the number....");
+  
+scanf ("%d", &a);
+  
+switch (a)
+    
+    {
+    
+case 1:
+      printf ("\n One");
+      break;
+    
+case 2:
+      printf ("\n Two");
+      break;
+    
+case 3:
+      printf ("\n Three");
+      break;
+    
+case 4:
+      printf ("\n Four");
+      break;
+    
+case 5:
+      printf ("\n Five");
+      break;
+    
+case 6:
+      printf ("\n Six");
+      break;
+    
+case 7:
+      printf ("\n Seven");
+      break;
+    case 8:
+      printf ("\n Eight");
+      break;
+    
+case 9:
+      printf ("\n Nine");
+      break;
+    case 10:printf("\n Ten");break;
+   default:printf("\n Enter the numbers betewwn (1-10)");
+ 
+}
+return 0;
+
+}  
