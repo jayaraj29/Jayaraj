43daa6eb2cab0bc5a90c20dff2ec1f7eaa92fc64
jayaraj29 <35688059+jayaraj29@users.noreply.github.com>
2018-02-28 14:21:23 +0530

    Create J55.C

---
+++ J55.C
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+ 
+int main()
+{
+    int a,b,c;
+    printf("\n Enter the numbers......");
+    scanf("%d%d",&a,&b);
+    c=a*b;
+    if(a%2==0)
+    printf("\n %d is even",c);
+    else
+    printf("\n %d is odd",c);
+    return 0;
+}
