946781ed61917c5ceec56b60d54751ee87f1bb1d
jayaraj29 <35688059+jayaraj29@users.noreply.github.com>
2018-03-07 14:36:28 +0530

    Update J75.C

--- J75.C
+++ J75.C
@@ -8,7 +8,7 @@ int main()
     printf("\n Enter the string");
     scanf("%s",&ch);
     a=(strlen(ch))/2;
-    if(a%2==0)
+    if(a%2!=0)
     {
         ch[a]='*';
         printf("\n %s",ch);
@@ -21,6 +21,7 @@ int main()
     }
     return 0;
 }
+
 
 
 
