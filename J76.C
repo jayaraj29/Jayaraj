15c8410ac4b2e1a28d395487475f78bc397a9bec
jayaraj29 <35688059+jayaraj29@users.noreply.github.com>
2018-03-07 14:55:06 +0530

    Create J76.C

---
+++ J76.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+int a,i,b=0;
+scanf("%d",&a);
+for(i=2;i<=a/2;i++)
+{
+if(a%i==0)
+{
+b=1;break;
+}
+}
+if(b==0)
+printf("\n no");
+else
+printf("\n yes");
+return 0;
+
+}
