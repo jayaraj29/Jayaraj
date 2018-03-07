e4c4907dbbe30523595ce8e023c9fad3ff3763af
jayaraj29 <35688059+jayaraj29@users.noreply.github.com>
2018-03-07 15:07:04 +0530

    Create J78.C

---
+++ J78.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+int a;
+printf("\n enter the number....");
+scanf("%d",&a);
+    if(a%13==0)
+    printf("\n Yes");
+    else
+    printf("\n NO");
+    return 0;
+
+}
