842dba33c5438504d3c768930ed2f789b482f471
jayaraj29 <35688059+jayaraj29@users.noreply.github.com>
2018-03-07 14:02:45 +0530

    Update J73.C

--- J73.C
+++ J73.C
@@ -5,8 +5,8 @@ int main()
     int a,b,c,d,e;
     printf("\n Enter the number....");scanf("%d",&a);
     printf("\n Enter the ramge....");scanf("%d%d",&b,&c);
-    d=(b<=a&&a<=c);
-    e=(b>=a&&a>=c);
+    d=(b<a&&a<c);
+    e=(b>a&&a>c);
     if(d||e)
     printf("\n Yes");
     else
